d58b39ea20f9d996e089e178b4dc26fc9f92eb56
Dhiaa <joseph.zebbiche@gmail.com>
2021-06-29 23:39:53 +0100

    Add names for the tables

--- covidapp/models.py
+++ covidapp/models.py
@@ -10,6 +10,8 @@ def user_loader(user_id):
     return Medecin.query.get(int(user_id))
 
 class Autoritaire(db.Model, UserMixin):
+    __tablename__ = "autoritaires"
+
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(20), nullable=False)
     prenom = db.Column(db.String(20), nullable= False)
@@ -20,6 +22,8 @@ class Autoritaire(db.Model, UserMixin):
         return f"Autoritaire('{self.nom}', '{self.prenom}', '{self.email}')"
 
 class Medecin(db.Model, UserMixin):
+    __tablename__ = "medecins"
+
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(20), nullable=False)
     prenom = db.Column(db.String(20), nullable= False)
@@ -30,6 +34,8 @@ class Medecin(db.Model, UserMixin):
         return f"Medecin('{self.nom}', '{self.prenom}', '{self.email}')"
 
 class ProfilPatient(db.Model):
+    __tablename__ = "patients"
+
     code = db.Column(db.Integer, primary_key=True)
     age = db.Column(db.Integer, nullable=False)
     sex = db.Column(db.String(1), nullable=False)
@@ -41,8 +47,11 @@ class ProfilPatient(db.Model):
         return f"Profil patient('{self.code}')"
 
 class Variant(db.Model):
+    __tablename__ = "variants"
+
     #nomScientifique = db.Column(db.String(50), nullable=False)
     whoLebel = db.Column(db.String(15), primary_key=True)
+    firstIdentified = db.Column(db.String(30), nullable=False)
 
     def __repr__(self):
         return f"Variant('{self.whoLebel}')"
@@ -56,12 +65,16 @@ class ProfilPatient_Variant(db.Model):
         return f"ProfilCode: {self.profilCode}, VariantID: {self.variantID}, Degre severite: {self.degreSeverite}"
 
 class UtilisateurMobile(db.Model):
+    __tablename__ = "utilisateurs"
+
     id = db.Column(db.Integer, primary_key=True)
     
     def __repr__(self):
         return f"UtilisateurMobile('{self.id}'"
   
 class Vaccin(db.Model):
+    __tablename__ = "vaccins"
+
     id = db.Column(db.Integer, primary_key=True)
     designation = db.Column(db.String(50), nullable=False)
     paysFabrication = db.Column(db.String(20), nullable=False)
@@ -70,22 +83,30 @@ class Vaccin(db.Model):
         return f"Vaccin('{self.designation}'"
 
 class Zone(db.Model):
+    __tablename__ = "zones"
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     population = db.Column(db.Integer, primary_key=True, nullable=False)
 
     def __repr__(self):
         return f"Zone: {self.id}"
 
-'''
 class CentreVaccination(db.Model):
+    __tablename__ = "centres"
+
     id = db.Column(db.Integer, primary_key=True)
    
 class DemandeVaccination(db.Model):
+    __tablename__ = "demandes"
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
 
 class Vaccination(db.Model):
+    __tablename__ = "vaccinations"
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
 
 class Convocation(db.Model):
+    __tablename__ = "convocations"
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-'''
