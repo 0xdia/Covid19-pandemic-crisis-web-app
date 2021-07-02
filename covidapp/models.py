65d940785fb2cb1040367dd3ecc0111d28909822
Dhiaa <joseph.zebbiche@gmail.com>
2021-07-02 23:47:07 +0100

    Complete database model

--- covidapp/models.py
+++ covidapp/models.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from covidapp import db, login_manager
 from flask_login import UserMixin
 
+# Add constraints cchecking later when having time
+
 @login_manager.user_loader
 def user_loader(user_id):
     if Autoritaire:
@@ -21,17 +23,64 @@ class Autoritaire(db.Model, UserMixin):
     def __repr__(self):
         return f"Autoritaire('{self.nom}', '{self.prenom}', '{self.email}')"
 
+class Zone(db.Model):
+    __tablename__ = "zones"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    nom = db.Column(db.String(40), nullable=False)
+    wilaya = db.Column(db.String(40), nullable=False)
+    # population = db.Column(db.Integer, primary_key=True, nullable=False)
+    centre = db.relationship('CentreVaccination', backref='zone', uselist=False)
+
+    def __repr__(self):
+        return f"<Zone('{self.id}', '{self.nom}', '{self.wilaya}')>"
+
+class CentreVaccination(db.Model):
+    __tablename__ = "centres"
+
+    id = db.Column(db.Integer, primary_key=True)
+    zone_id = db.Column(db.Integer, db.ForeignKey('zones.id'), nullable=False)
+
+    meds = db.relationship('Medecin', backref='centrevac')
+
+    def __repr__(self):
+        return f"<CentreVaccination('{self.id}', '{self.zone_id}')>"
+ 
 class Medecin(db.Model, UserMixin):
     __tablename__ = "medecins"
 
     id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(20), nullable=False)
-    prenom = db.Column(db.String(20), nullable= False)
+    nom_complet = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+    centre_id = db.Column(db.Integer, db.ForeignKey('centres.id'), nullable=False)
+
     def __repr__(self):
-        return f"Medecin('{self.nom}', '{self.prenom}', '{self.email}')"
+        return f"<Medecin('{self.nom_complet}', '{self.email}', '{self.centre_id}')>"
+
+class Variant(db.Model):
+    __tablename__ = "variants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    whoLabel = db.Column(db.String(15), nullable=False, unique=True)
+    firstIdentified = db.Column(db.String(30), nullable=False)
+
+    patients = db.relationship('ProfilPatient', backref='varnt', lazy='dynamic')
+
+    def __repr__(self):
+        return f"<Variant('{self.id}', '{self.whoLabel}', '{self.firstIdentified}')>"
+
+class UtilisateurMobile(db.Model):
+    __tablename__ = "utilisateurs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nom_complet = db.Column(db.String(60), nullable=False)
+    patients = db.relationship('ProfilPatient', backref='utilisateurMobile')
+    
+    def __repr__(self):
+        return f"<UtilisateurMobile('{self.id}', '{self.nom_complet}')>"
+ 
 
 class ProfilPatient(db.Model):
     __tablename__ = "patients"
@@ -39,39 +88,22 @@ class ProfilPatient(db.Model):
     code = db.Column(db.Integer, primary_key=True)
     age = db.Column(db.Integer, nullable=False)
     sex = db.Column(db.String(1), nullable=False)
+    nom_complet = db.Column(db.String(60), nullable=False)
     degreVulnerabilite = db.Column(db.Integer, nullable=False) 
-    utilisateurMobileID = db.Column(db.Integer, db.ForeignKey('UtilisateurMobile.id'), nullable=False)
-    zoneID = db.Column(db.Integer, db.ForeignKey('Zone.id'), nullable=False)
+
+    utilisateur_id = db.Column(db.Integer, db.ForeignKey('utilisateurs.id'), nullable=False)
+
+    variant_id = db.Column(db.Integer, db.ForeignKey('variants.id'))
+    degreSeverite = db.Column(db.Integer)
+
+    demande = db.relationship('DemandeVaccination', backref='patient', uselist=False)
+
+    zone_id = db.Column(db.Integer, db.ForeignKey('zones.id'), nullable=False)
 
     def __repr__(self):
-        return f"Profil patient('{self.code}')"
+        return f"<Profil patient('{self.code}', '{self.nom_complet}', '{self.age}', '{self.sex}')"
+ 
 
-class Variant(db.Model):
-    __tablename__ = "variants"
-
-    #nomScientifique = db.Column(db.String(50), nullable=False)
-    whoLebel = db.Column(db.String(15), primary_key=True)
-    firstIdentified = db.Column(db.String(30), nullable=False)
-
-    def __repr__(self):
-        return f"Variant('{self.whoLebel}')"
-
-class ProfilPatient_Variant(db.Model):
-    degreSeverite = db.Column(db.Integer, nullable=False)
-    profilCode = db.Column(db.Integer, db.ForeignKey('ProfilPatient.code'), primary_key=True)
-    variantID = db.Column(db.String, db.ForeignKey('Variant.id'), primary_key=True)
-
-    def __repr__(self):
-        return f"ProfilCode: {self.profilCode}, VariantID: {self.variantID}, Degre severite: {self.degreSeverite}"
-
-class UtilisateurMobile(db.Model):
-    __tablename__ = "utilisateurs"
-
-    id = db.Column(db.Integer, primary_key=True)
-    
-    def __repr__(self):
-        return f"UtilisateurMobile('{self.id}'"
-  
 class Vaccin(db.Model):
     __tablename__ = "vaccins"
 
@@ -80,33 +112,34 @@ class Vaccin(db.Model):
     paysFabrication = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return f"Vaccin('{self.designation}'"
+        return f"<Vaccin('{self.id}', '{self.designation}', '{self.paysFabrication}')>"
 
-class Zone(db.Model):
-    __tablename__ = "zones"
-
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    population = db.Column(db.Integer, primary_key=True, nullable=False)
-
-    def __repr__(self):
-        return f"Zone: {self.id}"
-
-class CentreVaccination(db.Model):
-    __tablename__ = "centres"
-
-    id = db.Column(db.Integer, primary_key=True)
-   
+  
 class DemandeVaccination(db.Model):
     __tablename__ = "demandes"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    code_patient = db.Column(db.Integer, db.ForeignKey("patients.code"), nullable=False)
+
+    vaccination = db.relationship('Vaccination', backref='demande', uselist=False)
+
+    def __repr__(self):
+        return f"<DemandeVaccination('{self.id}', '{self.code_patient}')>"
+
 
 class Vaccination(db.Model):
     __tablename__ = "vaccinations"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    demande_id = db.Column(db.Integer, db.ForeignKey("demandes.id"), nullable=False, unique=True) 
+
+    def __repr__(self):
+        return f"<Vaccination('{self.id}', '{self.demande_id}')>"
 
 class Convocation(db.Model):
     __tablename__ = "convocations"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return f""
